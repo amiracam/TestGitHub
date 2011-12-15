9954f14b850ff7dadc2b15642626b7604cbe7900
Charles Monteiro <charles@nycsmalltalk.org>
2011-12-15 16:43:57 -0500

    First commit

---
+++ test1.rb
@@ -0,0 +1,20 @@
+class Test1
+  # To change this template use File | Settings | File Templates.
+
+  #Pre-requisites
+
+
+  #Instance Variables
+
+
+  #Class Variables
+
+
+  #Methods
+
+  def hello_world
+    puts 'hello world'
+  end
+
+
+end
